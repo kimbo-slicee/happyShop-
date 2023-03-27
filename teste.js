9ce2544f80cfb72a7852576423accdb46bdc447b
kimbo-slicee <112892565+kimbo-slicee@users.noreply.github.com>
2023-03-27 17:51:43 +0000

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,7 @@
+function hello(){
+  return 'hello mohammed';
+}
+console.log(hello());
+cont users=[user_1,user_2,user_3,user_4];
+let teste=users.map((ele,index)=>ele==='user_3');
+console.log(teste);
